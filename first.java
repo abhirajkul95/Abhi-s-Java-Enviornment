280f4cb9f94d433f58a0172354aad01144964e4e
abhiraj <abhirajkul95@gmail.com>
2026-07-29 10:52:11 +0530

    input code

--- first.java
+++ first.java
@@ -1,7 +1,16 @@
+import java.util.Scanner;
 public class first
 {
    public static void main(String args[])
    {
-     System.out.println("Hello World");
+     System.out.println("Enter your Roll no");
+     Scanner input = new Scanner(System.in);
+     int Rollno = input.nextInt();
+     System.out.println("Enter Your Name");
+     input.nextLine();
+     String name = input.nextLine();
+     System.out.println("your name is "+ name);
+     System.out.println("Your Roll no is " + Rollno);
+     input.close();
    }
 }
