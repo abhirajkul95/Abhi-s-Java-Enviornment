353cb8cc50d77c93357737ddd6ff910e3b0909c2
abhiraj <abhirajkul95@gmail.com>
2026-08-25 15:52:24 +0530

    did some changes in the linear search with recursion code

--- linearwithrec.java
+++ linearwithrec.java
@@ -1,19 +1,20 @@
 public class linearwithrec {
     public static void main(String[] args) {
         int [] arr={4,7,22,78,89};
-        System.out.println(linear(arr,0,78));
+        System.out.println(linear(arr,0,89));
     }
 
     static int linear(int[] arr,int index,int target)
     {
+        if(index==arr.length)
+        {
+            return -1;
+        }
         if(target==arr[index])
         {
             return index;
         } 
-        if(target!=arr[index])
-        {
-            return -1;
-        }
+
         
       return linear(arr,index+1,target);
     }
