bc1020aab0f569ffe8898113112761882efdfb3b
abhiraj <abhirajkul95@gmail.com>
2026-09-21 13:40:45 +0530

    added code for maze with obstacles

---
+++ mazewithobstacles.java
@@ -0,0 +1,28 @@
+public class mazewithobstacles{
+public static void main(String[] args) {
+    prog("",3,3);
+}
+
+static void prog(String p,int r,int c)
+{
+    if(r==1&&c==1)
+    {
+        System.out.println(p);
+        return;
+    }
+
+    if(r==2&&c==2)
+    {
+        return;
+    }
+
+    if(r>1)
+    {
+        prog(p+'D',r-1,c);
+    }
+    if(c>1)
+    {
+        prog(p+'R',r,c-1);
+    }
+}
+}
